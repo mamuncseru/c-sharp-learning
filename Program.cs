47c7cbb549ad0cae3e0762f3f51943b6b40247ea
Md. Abdullah Al Mamun <mamun.cseian@gmail.com>
2022-02-16 01:23:30 +0600

    Hello world program running

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+// // Hello World! program
+namespace HelloWorld
+{
+    class Program {         
+        static void Main(string[] args)
+        {
+            // byte number = 2;
+            // int count = 10;
+            // float totalPrice = 20.95f;
+            // char character = 'A';
+            // string firstName = "Morsh";
+            // bool isWorking = false;
+
+            // var number = 2;
+            // var count = 10;
+            // var totalPrice = 20.95f;
+            // var character = 'A';
+            // var firstName = "Morsh";
+            // var isWorking = false;
+            
+            // Console.WriteLine(number);
+            // System.Console.WriteLine(count);
+            // System.Console.WriteLine(totalPrice);
+            // System.Console.WriteLine(character);
+            // System.Console.WriteLine(firstName);
+            // System.Console.WriteLine(isWorking);
+
+            System.Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+            System.Console.WriteLine();
+        }
+    }
+}
