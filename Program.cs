213d66e723ed6cbf7193d98dd97a0ff58b633e73
Md. Abdullah Al Mamun <mamun.cseian@gmail.com>
2022-02-16 01:32:01 +0600

    Type Conversion

--- Program.cs
+++ Program.cs
@@ -27,8 +27,41 @@ namespace HelloWorld
             // System.Console.WriteLine(firstName);
             // System.Console.WriteLine(isWorking);
 
-            System.Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
-            System.Console.WriteLine();
+            // System.Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+            // System.Console.WriteLine();
+
+            // int b = 1000;
+            // byte i = (byte)b;
+            // System.Console.WriteLine(i);
+
+            // var number = "1234";
+            // int i = Convert.ToInt32(number);
+            // System.Console.WriteLine(i);
+
+            
+            // try
+            // {
+            //     var number = "1234";
+            //     byte b = Convert.ToByte(number);
+            //     System.Console.WriteLine(b);
+            // }
+            // catch (System.Exception)
+            // {
+                
+            //     System.Console.WriteLine("The number could not be converted");
+            // }
+            
+            try
+            {
+                string str = "true";
+                bool b = Convert.ToBoolean(str);
+                System.Console.WriteLine(b);
+            }
+            catch (System.Exception)
+            {
+                
+                System.Console.WriteLine("The number could not be converted");
+            }
         }
     }
 }
